1771438ede05a3d9b7c158d25a2ee73237804f34
Francisco Andrade <francisco@inspr.com>
2021-03-21 13:19:57 -0300

    Part 1 - Exercise 1.2

--- part1/src/App.js
+++ part1/src/App.js
@@ -12,9 +12,8 @@ const App = () => {
   return (
     <>
       <Header course={course} />
-      <Content part={part1} exercises={exercises1} />
-      <Content part={part2} exercises={exercises2} />
-      <Content part={part3} exercises={exercises3} />
+      <Content part={[part1,part2,part3]}
+              exercises={[exercises1,exercises2,exercises3]} />
       <Total total={exercises1 + exercises2 + exercises3} />
     </>
   )
@@ -30,9 +29,11 @@ const Header = (props) => {
 
 const Content = (props) => {
   return (
-    <p>
-        {props.part} {props.exercises}
-    </p>
+    <div>
+        <Part part={props.part[0]} exercises={props.exercises[0]} />
+        <Part part={props.part[1]} exercises={props.exercises[1]} />
+        <Part part={props.part[2]} exercises={props.exercises[2]} />
+    </div>
   )
 }
 
@@ -40,6 +41,14 @@ const Total = (props) => {
   return (
     <p>
       Number of exercises {props.total}
+    </p>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <p>
+      {props.part} {props.exercises}
     </p>
   )
 }
