9e74b2b6bd7c6ec88c092f88ae9bf0343124dff4
Francisco Andrade <francisco@inspr.com>
2021-04-01 09:03:18 -0300

    Part 1 - Exercise 1.6

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,68 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+const Button = ({handleClick, text}) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
+)
+
+const Stats = ({good, neutral, bad}) => (
+  <>
+    <p>Good {good}</p>
+    <p>Neutral {neutral}</p>
+    <p>Bad {bad}</p>
+  </>
+)
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
-  }
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)  
 
   return (
     <>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
+      <h2>Give feedback</h2>
+      <Button
+        handleClick={() => setGood(good + 1)}
+        text={"Good"}
+      />
+      <Button
+        handleClick={() => setNeutral(neutral + 1)}
+        text={"Neutral"}
+      />
+      <Button
+        handleClick={() => setBad(bad + 1)}
+        text={"Bad"}
+      />
+      <h2>Statistics</h2>
+      <Stats good={good} neutral={neutral} bad={bad}/>
     </>
   )
 }
-
-const Header = (props) => {
-  return (
-    <>
-      <h1>{props.course}</h1>
-    </>
-  )
-}
-
-const Content = (props) => {
-  return (
-    <div>
-        <Part part={props.parts[0].name} exercises={props.parts[0].exercises} />
-        <Part part={props.parts[1].name} exercises={props.parts[1].exercises} />
-        <Part part={props.parts[2].name} exercises={props.parts[2].exercises} />
-    </div>
-  )
-}
-
-const Total = (props) => {
-  return (
-    <p>
-      Number of exercises {props.parts[0].exercises + 
-                            props.parts[1].exercises +
-                            props.parts[2].exercises}
-    </p>
-  )
-}
-
-const Part = (props) => {
-  return (
-    <p>
-      {props.part} {props.exercises}
-    </p>
-  )
-}
-
 
 export default App
