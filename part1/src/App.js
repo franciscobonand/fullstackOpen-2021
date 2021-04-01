5985e7dddcd20eaa23faf3f44528f3a13998f33d
Francisco Andrade <francisco@inspr.com>
2021-04-01 16:08:53 -0300

    Part 1 - Exercise 1.12

--- part1/src/App.js
+++ part1/src/App.js
@@ -6,59 +6,28 @@ const Button = ({handleClick, text}) => (
   </button>
 )
 
-const Stat = ({text, value}) => (
-  <tr>
-    <th>{text}</th>
-    <th>{value}</th>
-  </tr>
-)
-
-const Statistics = ({good, neutral, bad}) => {
-  const totalVotes = good + neutral + bad
-  if(totalVotes > 0) {
-    const avg = (good - bad)/totalVotes
-    const positive = (good/totalVotes) * 100
-    
-    return (
-      <table>
-        <tbody>
-          <Stat text={"Good"} value={good}/>
-          <Stat text={"Neutral"} value={neutral}/>
-          <Stat text={"Bad"} value={bad}/>
-          <Stat text={"All"} value={totalVotes}/>
-          <Stat text={"Average"} value={avg}/>
-          <Stat text={"Positive"} value={(positive).toString() + " %"}/>
-        </tbody>
-      </table>
-    )
-  }
-
-  return <>No feedback given</>
-}
-
 const App = () => {
-  // save clicks of each button to its own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)  
+  const anecdotes = [
+    'If it hurts, do it more often',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+  ]
+   
+  const [selected, setSelected] = useState(0)
 
+  const random = (max) =>  Math.floor(Math.random() * max)
+
+  console.log()
   return (
     <>
-      <h2>Give feedback</h2>
+      <h2>{anecdotes[selected]}</h2>
       <Button
-        handleClick={() => setGood(good + 1)}
-        text={"Good"}
+        handleClick={() => setSelected(random(anecdotes.length))}
+        text={"Next anecdote"}
       />
-      <Button
-        handleClick={() => setNeutral(neutral + 1)}
-        text={"Neutral"}
-      />
-      <Button
-        handleClick={() => setBad(bad + 1)}
-        text={"Bad"}
-      />
-      <h2>Statistics</h2>
-      <Statistics good={good} neutral={neutral} bad={bad}/>
     </>
   )
 }
