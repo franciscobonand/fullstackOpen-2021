15a4791ce43f58d5acfdd60b869a22abc7e113cb
Francisco Andrade <francisco@inspr.com>
2021-04-06 18:10:28 -0300

    Part 2 - Exercise 2.9

--- part2/phonebook/src/index.js
+++ part2/phonebook/src/index.js
@@ -3,11 +3,14 @@ import React, { useState } from 'react'
 
 const App = () => {
   const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas',
-      number: '040-1234567'}
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
   ]) 
   const [ newName, setNewName ] = useState('')
   const [ newPhone, setNewPhone ] = useState('')
+  const [ newFilter, setNewFilter ] = useState('')
 
   const handleNameChange = (event) => {
     console.log(event.target.value);
@@ -17,6 +20,11 @@ const App = () => {
   const handlePhoneChange = (event) => {
     console.log(event.target.value);
     setNewPhone(event.target.value)
+  }
+
+  const handleFilterChange = (event) => {
+    console.log(event.target.value);
+    setNewFilter(event.target.value)
   }
 
   const handleFormSubmission = (event) => {
@@ -33,9 +41,25 @@ const App = () => {
     setPersons(persons.concat(newPers))
   }
 
+  const returnFilteredData = () => {
+    const filter = newFilter.toLowerCase()
+    const filtered = persons.filter(p => p.name.toLowerCase().includes(filter))
+    
+    return filtered.map(pers => 
+        <p key={pers.name}>
+          {pers.name} {pers.number}
+        </p>
+    )
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
+        <div>
+          filter shown with: <input value={newFilter} 
+                                    onChange={handleFilterChange}/>
+        </div>
+      <h2>Add a new</h2>
       <form onSubmit={handleFormSubmission}>
         <div>
           name: <input value={newName} onChange={handleNameChange}/>
@@ -48,13 +72,7 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {
-        persons.map(pers => 
-          <p key={pers.name}>
-            {pers.name} {pers.number}
-          </p>
-        )
-      }
+        {returnFilteredData()}
     </div>
   )
 }
