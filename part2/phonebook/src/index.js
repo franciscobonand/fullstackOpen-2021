e4511076b2c3375317bf8d03c6a780f2670fd2a3
Francisco Andrade <francisco@inspr.com>
2021-04-06 18:53:27 -0300

    Part 2 - Exercise 2.10

--- part2/phonebook/src/index.js
+++ part2/phonebook/src/index.js
@@ -8,24 +8,27 @@ const App = () => {
     { name: 'Dan Abramov', number: '12-43-234345' },
     { name: 'Mary Poppendieck', number: '39-23-6423122' }
   ]) 
-  const [ newName, setNewName ] = useState('')
-  const [ newPhone, setNewPhone ] = useState('')
   const [ newFilter, setNewFilter ] = useState('')
 
-  const handleNameChange = (event) => {
-    console.log(event.target.value);
-    setNewName(event.target.value)
-  }
-
-  const handlePhoneChange = (event) => {
-    console.log(event.target.value);
-    setNewPhone(event.target.value)
-  }
-
   const handleFilterChange = (event) => {
-    console.log(event.target.value);
     setNewFilter(event.target.value)
   }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+        <Filter filter={newFilter} handleChange={handleFilterChange}/>
+      <h2>Add a new</h2>
+        <PersonForm persons={persons} setPersons={setPersons}/>
+      <h2>Numbers</h2>
+        <People filter={newFilter} persons={persons}/>
+    </div>
+  )
+}
+
+const PersonForm = ({persons, setPersons}) => {
+  const [ newName, setNewName ] = useState('')
+  const [ newPhone, setNewPhone ] = useState('')
 
   const handleFormSubmission = (event) => {
     event.preventDefault()
@@ -39,40 +42,62 @@ const App = () => {
       number: newPhone
     }
     setPersons(persons.concat(newPers))
+    setNewName('')
+    setNewPhone('')
   }
 
-  const returnFilteredData = () => {
-    const filter = newFilter.toLowerCase()
-    const filtered = persons.filter(p => p.name.toLowerCase().includes(filter))
-    
-    return filtered.map(pers => 
-        <p key={pers.name}>
-          {pers.name} {pers.number}
-        </p>
-    )
+  return (
+    <form onSubmit={handleFormSubmission}>
+      <PersonName newName={newName} setNewName={setNewName}/>
+      <PersonPhone newPhone={newPhone} setNewPhone={setNewPhone}/>
+      <div>
+        <button type="submit">add</button>
+      </div>
+    </form>
+  )
+}
+
+const PersonName = ({newName, setNewName}) => {
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
   }
 
   return (
     <div>
-      <h2>Phonebook</h2>
-        <div>
-          filter shown with: <input value={newFilter} 
-                                    onChange={handleFilterChange}/>
-        </div>
-      <h2>Add a new</h2>
-      <form onSubmit={handleFormSubmission}>
-        <div>
-          name: <input value={newName} onChange={handleNameChange}/>
-        </div>
-        <div>
-          number: <input value={newPhone} onChange={handlePhoneChange}/>
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
-      <h2>Numbers</h2>
-        {returnFilteredData()}
+      name: <input value={newName} onChange={handleNameChange}/>
+    </div>
+  )
+}
+
+const PersonPhone = ({newPhone, setNewPhone}) => {
+  const handlePhoneChange = (event) => {
+    setNewPhone(event.target.value)
+  }
+
+  return (
+    <div>
+      number: <input value={newPhone} onChange={handlePhoneChange}/>
+    </div>
+  )
+}
+
+const People = ({filter, persons}) => {
+  const lcFilter = filter.toLowerCase()
+  const filtered = persons.filter(p => p.name.toLowerCase().includes(lcFilter))
+  
+  return filtered.map(pers => 
+      <p key={pers.name}>
+        {pers.name} {pers.number}
+      </p>
+  )
+}
+
+const Filter = ({filter, handleChange}) => {
+  return (
+    <div>
+      filter shown with: 
+        <input value={filter} 
+                onChange={handleChange}/>
     </div>
   )
 }
